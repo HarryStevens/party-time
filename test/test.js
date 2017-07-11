d6b51c723bd65167cfac65e0cb15d020b95a7bbe
HarryStevens <harryjosephstevens@gmail.com>
2017-07-11 23:37:46 +0530

    Add parties and properties

--- test/test.js
+++ test/test.js
@@ -1,52 +1,54 @@
 "use strict";
 
 var expect = require("chai").expect;
-var pc = require("../lib/party-time.js");
+var pt = require("../lib/party-time.js");
+
+console.log(pt.convert("Communist Party of India (Marxist)", false)); 
 
 describe("#partyConverter", function(){
 
 	it("should convert an acronym to a party greedily", function(){
-		var a = pc.convert("BJP");
-		var a = pc.convert("BJP", true);
+		var a = pt.convert("BJP");
+		var a = pt.convert("BJP", true);
 		expect(a).to.equal("Bharatiya Janata Party");
 	});
 
 	it("should convert an acronym to a party object", function(){
-		var a = pc.convert("BJP", false);
+		var a = pt.convert("BJP", false);
 		expect(a.abbr).to.equal("BJP");
 		expect(a.name).to.equal("Bharatiya Janata Party");
 	});
 
 	it("should convert a party to an acronym greedily", function(){
-		var a = pc.convert("Indian National Congress");
+		var a = pt.convert("Indian National Congress");
 		expect(a).to.equal("INC");
 	});
 
 	it("should convert a party name to a party object", function(){
-		var a = pc.convert("Indian National Congress", false);
+		var a = pt.convert("Indian National Congress", false);
 		expect(a.name).to.equal("Indian National Congress");
 		expect(a.abbr).to.equal("INC");
 	});
 
 	it("should take a third type argument", function(){
-		var a = pc.convert("BJP", true, "abbr");
-		var b = pc.convert("bjp", null, "abbr");
+		var a = pt.convert("BJP", true, "abbr");
+		var b = pt.convert("bjp", null, "abbr");
 		expect(a).to.equal("Bharatiya Janata Party");
 		expect(b).to.equal("Bharatiya Janata Party");
 	});
 	
 	it("should return variations", function(){
-		var a = pc.convert("Communist Party of India (Marxist)", false);
+		var a = pt.convert("Communist Party of India (Marxist)", false);
 		expect(a.variations.abbr[0]).to.equal("CPM");
 	});
 
 	it("should find variations", function(){
-		var a = pc.convert("All India Majlis-E-Ittehadul Muslimeen");
+		var a = pt.convert("All India Majlis-E-Ittehadul Muslimeen");
 		expect(a).to.equal("AIMIM");
 	});
 
 	it("should return the party if there are no matches", function(){
-		var a = pc.convert("Test");
+		var a = pt.convert("Test");
 		expect(a).to.equal("Test");
 	});
 	
