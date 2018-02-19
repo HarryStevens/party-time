1f1d0878baaae47ff9e5283f6f4e578768034eb6
HarryStevens <harryjosephstevens@gmail.com>
2018-02-19 17:31:48 +0530

    Add Bihar 2010 and sorting script

---
+++ sort.js
@@ -0,0 +1,6 @@
+var io = require("indian-ocean"),
+	jz = require("jeezy");
+
+var json = io.readDataSync("src/data/parties.json");
+
+io.writeDataSync("src/data/parties.json", jz.arr.sortBy(json, "name"), {indent: 2});
